2b4e06ceaf44d01cfd81322e84767a1fc86a943d
Benas <benas.sarachovas@gmail.com>
2023-06-17 04:53:10 +0100

    added responsive layout for menu items

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMenu(){
-    let first_page = document.getElementById('first-page');
+    let first_page = document.getElementById('first-page-intro');
     first_page.classList.add('fade-out')
 }
